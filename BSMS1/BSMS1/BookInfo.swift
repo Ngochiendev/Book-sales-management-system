39fae98fec90ce6c99e79ead4626edb01a8e82f0
陈欣 <95323362@qq.com>
2016-04-28 23:23:01 +0800

    -

---
+++ BSMS1/BSMS1/BookInfo.swift
@@ -0,0 +1,13 @@
+//
+//  BookInfo.swift
+//  BSMS1
+//
+//  Created by ChenXin on 16/4/28.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class BookInfo: NSObject {
+    
+}
