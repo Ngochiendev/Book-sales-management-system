0bb479ef8a6ae75c81e76b1ca0415beed4c3fa05
陈欣 <95323362@qq.com>
2016-04-28 23:23:46 +0800

    -

---
+++ BSMS2/BSMS2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BSMS2
+//
+//  Created by ChenXin on 16/4/28.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
