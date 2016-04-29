c2944867f9f74afa65aeb89e6551fe6a851d91ee
陈欣 <95323362@qq.com>
2016-04-29 21:31:30 +0800

    -

--- BSMS2/BSMS2/ViewController.swift
+++ BSMS2/BSMS2/ViewController.swift
@@ -16,6 +16,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        bookcells = [
+                     myModel(id: "1", image: "se", title: "《软件工程》", intro: "机械工业出版社/69.00元"),
+                     myModel(id: "2", image: "linux", title: "《鸟哥的Linux私房菜》", intro: "人民邮电出版社/88.00元"),
+                     myModel(id: "3", image: "algorithm", title: "《算法导论》", intro: "机械工业出版社/128.00元"),
+                     myModel(id: "4", image: "html", title: "《深入浅出HTML与CSS、XHTML》", intro: "东南大学出版社/98.00元"),
+                    myModel(id: "5", image: "java", title: "《Java核心技术·卷1：基础知识》", intro: "机械工业出版社/88.00元"),
+                    myModel(id: "6", image: "ios", title: "《Hacking and Securing iOS Applications》", intro: "McGraw-Hill Osborne Media/98.00元"),
+                    myModel(id: "7", image: "oc", title: "《Programming in Objective-C》", intro: " Addison-Wesley Professional/70.00元")
+                    
+        ]
     }
 
     override func didReceiveMemoryWarning() {
